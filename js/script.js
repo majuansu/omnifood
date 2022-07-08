2935bcbe44f345429069988ba9bac94e406fc090
SM <sm@SMs-MacBook-Air.local>
2022-07-09 09:11:06 +1000

    add browser support and fix flexbox gap in Safari

--- js/script.js
+++ js/script.js
@@ -32,6 +32,7 @@ allLinks.forEach((link) => {
       targetSection.scrollIntoView({ behavior: "smooth" });
     }
   });
+  // https://unpkg.com/smoothscroll-polyfill@0.4.4/dist/smoothscroll.min.js
 
   //////// Close mobile nav menu when window scrolling
   window.addEventListener("scroll", () => {
@@ -63,3 +64,22 @@ const obs = new IntersectionObserver(
   }
 );
 obs.observe(sectionHero);
+
+/////// Fixing flexbox gap property missing in some Safari versions
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
