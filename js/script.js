0ddf778b60029af08f741edce16505691408b7e4
SM <sm@SMs-MacBook-Air.local>
2022-07-08 21:34:39 +1000

    complete smooth scrolling effect when clicking links

--- js/script.js
+++ js/script.js
@@ -1,12 +1,40 @@
-// set the year of footer
+/////// Set the year of footer
 const year = document.querySelector(".current-year");
 year.textContent = new Date().getFullYear();
 
-// make mobile nav menu work
+/////// Make mbile nav menu button work
 const navbutton = document.querySelector(".btn-mobile-nav");
 const header = document.querySelector(".header");
 
 navbutton.addEventListener("click", () => {
   header.classList.toggle("nav-open");
   console.log("button is clicked");
+});
+
+/////// Smooth scrolling animation
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const href = link.getAttribute("href");
+
+    if (href === "#") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    }
+
+    if (href !== "#" && href.startsWith("#")) {
+      const targetSection = document.querySelector(href);
+      targetSection.scrollIntoView({ behavior: "smooth" });
+    }
+  });
+
+  //////// Close mobile nav menu when window scrolling
+  window.addEventListener("scroll", () => {
+    header.classList.remove("nav-open");
+  });
 });
